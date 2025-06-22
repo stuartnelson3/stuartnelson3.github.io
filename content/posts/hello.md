81ef7fa04c45f79fbc5f85c698001b2a9564f451
stuart nelson <hi@stuartnelson.xyz>
2025-06-22 16:47:47 +0200

    updates

--- content/posts/hello.md
+++
@@ -1,6 +0,0 @@
----
-title: "Hello"
-date: 2022-04-04T18:33:51+02:00
----
-
-test test what's going on
--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,6 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
 hugo -t smol # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-mv public docs
-
 # Go To Public folder
 # cd public
 
