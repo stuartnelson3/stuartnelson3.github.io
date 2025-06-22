0627abed8212b5f07422adfd0c86ed4df0c98b43
stuart nelson <hi@stuartnelson.xyz>
2025-06-22 22:16:55 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,7 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
 hugo -t smol # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
-# cd public
-
-# Add changes to git.
-# git add .
+git add .
 
 # Commit changes.
 msg="rebuilding site $(date)"
